d01a45a447d2d98f316cd9108e534b9133a454b6
Gennady Telegin <gtelegin@gmail.com>
2013-07-12 13:06:22 +0400

    move to upload directory. add english language. add readme

---
+++ upload/admin/language/english/tool/import_yml.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']     = 'Import YML';
+
+// Text
+$_['text_success']      = 'Products have been loaded successfully';
+
+// Entry
+$_['entry_restore']     = 'File:';
+$_['entry_description'] = 'Load products from YML file';
+$_['entry_force']       = 'Delete all data from the shop before import:';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to import YML!';
+
+// Button labels
+$_['button_import']     = 'Import';
+?>
