69a356fc97d6dd762d2c6bf7b3fa3ba6405e863c
Gennady Telegin <gtelegin@gmail.com>
2013-09-20 13:46:10 +0400

    Add English translation, fix localization names.

--- upload/admin/language/english/tool/import_yml.php
+++ upload/admin/language/english/tool/import_yml.php
@@ -4,11 +4,21 @@ $_['heading_title']     = 'Import YML';
 
 // Text
 $_['text_success']      = 'Products have been loaded successfully';
+$_['text_settings_success'] = 'Settings have been saved successfully';
 
 // Entry
 $_['entry_restore']     = 'File:';
+$_['entry_url']     	= 'URL:<br/><span class="help">You can specify URL instead of file</span>';
 $_['entry_description'] = 'Load products from YML file';
 $_['entry_force']       = 'Delete all data from the shop before import:';
+$_['entry_update']      = 'Update fields:';
+$_['entry_field_name']        = 'Name';
+$_['entry_field_description'] = 'Description';
+$_['entry_field_price'] 		= 'Price';
+$_['entry_field_image'] 		= 'Image';
+$_['entry_field_manufacturer'] = 'Manufacturer';
+$_['entry_field_attribute'] 	= 'Attributes';
+$_['entry_save_settings'] = 'Save settings without import (for cron task):';
 
 // Error
 $_['error_permission']    = 'Warning: You do not have permission to import YML!';
--- upload/admin/language/russian/tool/import_yml.php
+++ upload/admin/language/russian/tool/import_yml.php
@@ -12,12 +12,12 @@ $_['entry_url']     	= 'URL:<br/><span class="help">Вместо файла мо
 $_['entry_description'] = 'Загрузка продуктов с файла YML';
 $_['entry_force']       = 'Удалить все данные из магазина перед импортом:';
 $_['entry_update']      = 'Обновить поля:';
-$_['entry_name']        = 'Название товара';
-$_['entry_description'] = 'Описание';
-$_['entry_price'] 		= 'Цена';
-$_['entry_image'] 		= 'Изображение товара';
-$_['entry_manufacturer'] = 'Производитель';
-$_['entry_attribute'] 	= 'Атрибуты';
+$_['entry_field_name']        = 'Название товара';
+$_['entry_field_description'] = 'Описание';
+$_['entry_field_price'] 		= 'Цена';
+$_['entry_field_image'] 		= 'Изображение товара';
+$_['entry_field_manufacturer'] = 'Производитель';
+$_['entry_field_attribute'] 	= 'Атрибуты';
 $_['entry_save_settings'] = 'Сохранить настройки без импорта:';
 
 // Error
